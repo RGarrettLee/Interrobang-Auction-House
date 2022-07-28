996efd272e7b5c0b4e2f7379cd228d2ade229c67
Sachmilan <sachmilansingh@gmail.com>
2022-07-28 11:05:30 -0600

    pushing the resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,41 +1,68 @@
-const { Profile } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { user,auctionItem } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    profiles: async () => {
+    users: async () => {
+      return user.find();
+    },
+
+    auctionItems: async () => {
       return Profile.find();
     },
 
-    profile: async (parent, { profileId }) => {
-      return Profile.findOne({ _id: profileId });
+    user: async () => {
+      return user.findOne({_id:userId});
+    },
+
+    auctionItem: async () => {
+      return aucyion.findOne({_id:auctionItemId});
     },
   },
 
   Mutation: {
-    addProfile: async (parent, { name }) => {
-      return Profile.create({ name });
+    adduser: async (parent, { name, email, password, address }) => {
+
+      const user = user.create({ name,email,password,address });
+      const token = signToken(user);
+      return {user,token};
     },
-    addSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        {
-          $addToSet: { skills: skill },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
+
+    login: async (parent, { email, password }) => {
+      // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      // If email and password are correct, sign user into the application with a JWT
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
     },
-    removeProfile: async (parent, { profileId }) => {
-      return Profile.findOneAndDelete({ _id: profileId });
+
+    removeuser: async (parent, { profileId }) => {
+      return user.findOneAndDelete({ _id: profileId });
     },
-    removeSkill: async (parent, { profileId, skill }) => {
-      return Profile.findOneAndUpdate(
-        { _id: profileId },
-        { $pull: { skills: skill } },
-        { new: true }
-      );
+
+    addauctionItem: async (parent,{name, images, closingDate, price, highestBidder})=>{
+      return auctionItem.create({ name, images, closingDate, price, highestBidder});
+    },
+
+    removeauctionItem: async(parent,{auctionItemId}) =>{
+      return auctionItem.findOneAndDelete({_id: auctionItemId})
     },
   },
 };
