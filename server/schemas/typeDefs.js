f3763156d324411a97769032d8c3804ba31eed43
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-29 12:05:41 -0400

    fixed same name query issue and created new queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,8 +23,10 @@ const typeDefs = gql`
   }
 
   type Query {
-    auctionItem:[auctionItem]
-    user:[User]
+    allAuctionItem:[auctionItem]
+    allUsers:[User]
+    oneAuctionItem(_id: String!): auctionItem
+    oneUser(username: String!): User
   }
   type Mutation {
     addUser(username: String!, email: String!, password: String!, address: String!,): Auth
