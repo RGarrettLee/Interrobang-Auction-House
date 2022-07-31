d2978a0f37d8c3a96e0e7536e6bb1643810fe881
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-07-31 12:46:43 -0400

    repaired menu functions to admin and login

--- client/src/components/elements/Menu/Menu.jsx
+++ client/src/components/elements/Menu/Menu.jsx
@@ -75,6 +75,20 @@ export default function DropMenu() {
                     justifyContent: 'center',
                     color: isActive ? '#ff7043' : '#ffa270',
                 })} >Register</NavLink></MenuItem>
+                <MenuItem onClick={handleClose}><NavLink to="/admin" style={({ isActive }) => ({
+                    fontFamily: 'Montserrat Alternates',
+                    fontSize: '20px',
+                    textDecoration: 'none',
+                    justifyContent: 'center',
+                    color: isActive ? '#ff7043' : '#ffa270',
+                })} >Admin</NavLink></MenuItem>
+                <MenuItem onClick={handleClose}><NavLink to="/login" style={({ isActive }) => ({
+                    fontFamily: 'Montserrat Alternates',
+                    fontSize: '20px',
+                    textDecoration: 'none',
+                    justifyContent: 'center',
+                    color: isActive ? '#ff7043' : '#ffa270',
+                })} >Login</NavLink></MenuItem>
             </Menu>
         </div>
     );
