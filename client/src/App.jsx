e422c3783db973df4d840332a86319a3fd77330a
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-08-01 17:02:02 -0400

    creating apollo provider connection to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,26 +13,62 @@ import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import theme from './components/styles/Styles';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  //uri: 'http://localhost:3001/',
+  //link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <div>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Home />} ></Route>
-            <Route path='/my-account' element={<Dashboard />}></Route>
-            <Route path='/item-details' element={<ItemDetails />}></Route>
-            <Route path='/login' element={<Login />}></Route>
-            <Route path='/logout' element={<ItemDetails />}></Route>
-            <Route path="/notfound" element={<NotFound />} status={404} />
-            <Route path="/register" element={<Register />} status={404} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <div>
+            <Header />
+            <Routes>
+              <Route path='/' element={<Home />} ></Route>
+              <Route path='/my-account' element={<Dashboard />}></Route>
+              <Route path='/item-details' element={<ItemDetails />}></Route>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/logout' element={<ItemDetails />}></Route>
+              <Route path="/notfound" element={<NotFound />} status={404} />
+              <Route path="/register" element={<Register />} status={404} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
