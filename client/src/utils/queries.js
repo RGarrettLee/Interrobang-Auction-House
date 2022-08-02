61b00e92bd17e7d226cb8a13675022d65083fe6f
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-08-02 01:22:12 -0400

    added single auction item query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -4,6 +4,39 @@ export const QUERY_AUCTION_ITEMS = gql`
     query GetAuctionItems {
         allAuctionItems {
             name
+            images
+            itemDescription
+            artistName
+            origin
+            artistInfo
+            dateProduced
+            size
+            artMedium
+            valuation
+            openingBid
+            currentBidValue
+            closingDate
+            lot
+        }
+    }
+`
+
+export const QUERY_AUCTION_ITEM = gql`
+    query GetAuctionItem($name: String!) {
+        oneAuctionItem(name: $name) {
+            images
+            itemDescription
+            artistName
+            origin
+            artistInfo
+            dateProduced
+            size
+            artMedium
+            valuation
+            openingBid
+            currentBidValue
+            closingDate
+            lot
         }
     }
 `
