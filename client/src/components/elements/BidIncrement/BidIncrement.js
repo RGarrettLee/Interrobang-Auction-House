f52a32439995e05be1c43173e9c74ec86ec1e787
Sachmilan <sachmilansingh@gmail.com>
2022-08-02 22:05:44 -0600

    pushing the updates in register.js

--- client/src/components/elements/BidIncrement/BidIncrement.js
+++ client/src/components/elements/BidIncrement/BidIncrement.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const BidIncrement = () => {
   return (
-      <div>BidIncrement</div>
+    <div>
+            {/* <div>BidIncrement</div>
+            <div>
+              {loggedIn?(
+                <Link to=""></Link>
+              ):(
+                <link to=""></link>
+              )}
+            </div> */}
+    </div>
+
   )
 }
 
--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -4,7 +4,8 @@ import { Box, Typography, Button, TextField } from '@mui/material';
 import { ADD_USER } from '../../../utils/mutation';
 import Auth from '../../../utils/auth';
 
-const Register = (e) => {
+function Register() {
+
     const[formState, setFormState] = useState({
         email:'',
         password:'',
@@ -36,59 +37,16 @@ const Register = (e) => {
         }
     };
 
-
-return(
-    <div >
-    {/* {data ? (
-        <p>
-          Success! You may now head{' '}
-          <Link to="/">back to the homepage.</Link>
-        </p>
-      ) : ( */}
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-        <form onSubmit={handleFormSubmit}>
-          <input
-            className="form-input"
-            placeholder="Your email"
-            name="email"
-            type="email"
-            onChange={handleChange}
-          />
-          <input
-            className="form-input"
-            placeholder="******"
-            name="password"
-            type="password"
-            onChange={handleChange}
-          />
-          <button
-            className="btn btn-block btn-primary"
-            style={{ cursor: 'pointer' }}
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-      
-
-      {error && (
-        <div className="my-3 p-3 bg-danger text-white">
-          {error.message}
-          {console.log(JSON.stringify(error, null, 2))}
-        </div>
-      )}
-    </div>
-);
+    return (
+        <Box className="form" sx={{ marginTop: '80px', padding:'10px', textAlign: "center", flexGrow: 1 }}>
+            <Typography fontFamily="Montserrat Alternates" variant="h3" >Register</Typography><br />
+            <TextField margin="dense" id="Email" label="Email" variant="filled" onChange={handleChange}/><br />
+            <TextField margin="dense" id="password" label="Password" variant="filled" onChange={handleChange}/><br />
+            <br /><br />
+            <Button variant="contained" type="submit" color="secondary" onSubmit={handleFormSubmit}>Register</Button>
+        </Box>
+    )
 }
+
 
 export default Register;
